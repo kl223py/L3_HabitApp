cf870a7a48f37749c936b8ab212dae064f9dc504
Kevin Lissov <kl223py@student.lnu.se>
2025-10-17 00:00:38 +0200

    Add floating action button to habits screen for adding new habits

--- HabitFlow/app/(tabs)/habits.tsx
+++ HabitFlow/app/(tabs)/habits.tsx
@@ -1,10 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity} from "react-native";
 import React from "react";
 
 export default function habits() {
   return (
     <View style={styles.container}>
-
+      <TouchableOpacity
+      style={styles.addButton}
+      activeOpacity={0.5}
+      >
+        <Text style={styles.addButtonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,5 +20,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
     backgroundColor: '#f5f5f5',
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 20,
+    alignSelf: 'center',
+    backgroundColor: '#A58BFF',
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+  },
+  addButtonText: {
+    fontSize: 32,
+    color: '#fff',
+    fontWeight: '300',
   }
 });
